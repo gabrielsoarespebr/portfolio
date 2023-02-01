dcd204169bbb75b932a056a1f6e683d8525b21d1
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2023-02-01 09:19:22 -0300

    feat: Add resume

--- src/services/userInfo.js
+++ src/services/userInfo.js
@@ -394,7 +394,7 @@ export const userInfo = {
 
         github: "https://github.com/gabrielsoarespebr",
         linkedin: "https://www.linkedin.com/in/gabrielsoarespebr/",
-        resume: "https://www.linkedin.com/ambry/?x-li-ambry-ep=AQI8GZJ2gdb4GAAAAYXXMH0kNw7vylQeIh3jeF_pYf9q4CS-m7WPHAkF_eCpSSXMef5egJ1ST1pmkGEN-L0mRCRKlcly8l6HFUjMJ_ZzGX2oEm9oxOYmqUO7U-wuk6flooYwZQrn8j9deWJEaR8vOHrI_42jKIwsnTFDLy3DxXMLQz2qNtPPnNJkC6H6QmgF3Uwq7aq5qCduRfnXOvXCf_1vm-CmL-Mf58EyTKtuK9oFsUwEwn9CIOf9P7wiFHxyTEAnAJOoWxE7zwM1x7KFJzCTrlDfaljLtpkDe0SxjrxpsPXxfWFyKXNLviVpCHiGwfplF48pthWNO7WHya4zP5C8XPeJmdUB2LBEFPT-qSWX3yFYYacnSsbGsjIfT1eRAqVFoQxykmkjJFpDIcusW34G6uycDr6mqMzPIBw-7KX3ckGA0yaHwCTa12gp22tKnhp9WJt_vKeZk_2c4nRMAhHly2RpMYhLnj9PTV5rqKoQnZ7m8WwFCNCdhtR74bbz7KEi6YNhutd95CRk0Olid_h9T52LAiLmajKPx1KRUHEkq7buzfuXemWvnZedQDsMgCeM2Q&x-ambry-um-filename=Profile.pdf"
+        resume: "./src/assets/doc/Curriculo - Gabriel Soares.pdf"
 
     }
 };
